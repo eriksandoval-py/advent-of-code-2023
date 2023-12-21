075ba9c5a93a2e1a4c3ed4fdec0f076e9cc1ca9c
Erik Sandoval <eriksandoval.py@gmail.com>
2023-12-20 21:14:41 -0600

    Day 1 Part 1 completed.

---
+++ day1/src/Part1.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.regex.Pattern;
+
+public class Part1 {
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new FileReader("input1.txt"))) {
+            long sum = 0;
+
+            String regex = "(\\d)";
+            Pattern pattern = Pattern.compile(regex);
+
+            for (String line; (line = reader.readLine()) != null;) {
+                java.util.regex.Matcher matcher = pattern.matcher(line);
+                String firstDigit = null;
+                String lastDigit = null;
+                while (matcher.find()) {
+                    if (firstDigit == null) {
+                        firstDigit = matcher.group();
+                    }
+                    lastDigit = matcher.group();
+                }
+                if (firstDigit != null) {
+                    String digit = firstDigit + (lastDigit != null ? lastDigit : firstDigit);
+                    sum += Long.parseLong(digit);
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
